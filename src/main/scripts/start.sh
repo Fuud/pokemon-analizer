d2094cbc851a1756329d258e06d2d9de9a221a05
Fedor Bobin <fuudtorrentsru@gmail.com>
2016-08-19 11:04:48 +0300

    jmx

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,3 +1,8 @@
-JMX_PARAMS="-Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+JMX_PARAMS=""
+JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.port=12345"
+JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.rmi.port=12345"
+JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.authenticate=false"
+JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.ssl=false"
+JMX_PARAMS="$JMX_PARAMS -Djava.rmi.server.hostname=$EXTERNAL_HOST_IP"
 
 java -cp "./target/packaged/lib/*" $JMX_PARAMS $ADDITIONAL_JAVA_OPTS fuud.Main
