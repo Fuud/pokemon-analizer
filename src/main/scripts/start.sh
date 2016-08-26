3a60995b38b0f118fd2acfd98e65f53457c1ac7f
Fedor Bobin <fuudtorrentsru@gmail.com>
2016-08-26 17:13:13 +0300

    logging

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -5,4 +5,11 @@ JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.authenticate=false"
 JMX_PARAMS="$JMX_PARAMS -Dcom.sun.management.jmxremote.ssl=false"
 JMX_PARAMS="$JMX_PARAMS -Djava.rmi.server.hostname=$EXTERNAL_HOST_IP"
 
-java -cp "./target/packaged/lib/*" $JMX_PARAMS $ADDITIONAL_JAVA_OPTS fuud.Main
+echo " "                              >> /var/log/pokemons/pokemons.stdout
+echo " "                              >> /var/log/pokemons/pokemons.stdout
+echo "==============================" >> /var/log/pokemons/pokemons.stdout
+echo "  START"                        >> /var/log/pokemons/pokemons.stdout
+date                                  >> /var/log/pokemons/pokemons.stdout
+echo "==============================" >> /var/log/pokemons/pokemons.stdout
+
+java -cp "./target/packaged/lib/*" $JMX_PARAMS $ADDITIONAL_JAVA_OPTS fuud.Main >> /var/log/pokemons/pokemons.stdout
