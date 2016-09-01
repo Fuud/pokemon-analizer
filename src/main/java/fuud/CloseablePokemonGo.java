ca41670f3671c137a77279e73d2c146905e8b271
Fedor Bobin <fuudtorrentsru@gmail.com>
2016-09-01 11:25:30 +0300

    workaround: close thread

--- src/main/java/fuud/CloseablePokemonGo.java
+++ src/main/java/fuud/CloseablePokemonGo.java
@@ -8,26 +8,38 @@ import com.pokegoapi.main.RequestHandler;
 import com.pokegoapi.util.Time;
 import okhttp3.OkHttpClient;
 
-import java.io.Closeable;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class CloseablePokemonGo extends PokemonGo implements Closeable{
+public class CloseablePokemonGo extends PokemonGo implements Closeable {
+    private static final AtomicInteger threadCounter = new AtomicInteger();
 
     public CloseablePokemonGo(CredentialProvider credentialProvider, OkHttpClient client) throws LoginFailedException, RemoteServerException {
         super(client);
-        login(credentialProvider);
+
+        try{
+            login(credentialProvider);
+        }catch (Throwable t){
+            close();
+            throw t;
+        }
     }
 
     @Override
-    public void close() throws IOException {
-        try{
-            final Field asyncHttpThreadField = RequestHandler.class.getDeclaredField("asyncHttpThread");
-            asyncHttpThreadField.setAccessible(true);
-            final Object asyncHttpThread = asyncHttpThreadField.get(getRequestHandler());
-            Thread.class.cast(asyncHttpThread).interrupt();
-        }catch (Exception e){
-            throw new IOException(e);
+    public void close() {
+        try {
+            final Thread thread = getThread();
+            thread.interrupt();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private Thread getThread() throws NoSuchFieldException, IllegalAccessException {
+        final Field asyncHttpThreadField = RequestHandler.class.getDeclaredField("asyncHttpThread");
+        asyncHttpThreadField.setAccessible(true);
+        final Object asyncHttpThread = asyncHttpThreadField.get(getRequestHandler());
+        return Thread.class.cast(asyncHttpThread);
     }
 }
